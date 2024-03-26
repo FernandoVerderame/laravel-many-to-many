345a4a8a5856cc54ce6ad03964723c70e46af1d8
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-26 16:59:10 +0100

    added attach method

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -90,6 +90,10 @@ class ProjectController extends Controller
 
         $project->save();
 
+        if (Arr::exists($data, 'technologies')) {
+            $project->technologies()->attach($data['technologies']);
+        }
+
         return to_route('admin.projects.show', $project->id)->with('type', 'success')->with('message', 'New project successfully created!');
     }
 
